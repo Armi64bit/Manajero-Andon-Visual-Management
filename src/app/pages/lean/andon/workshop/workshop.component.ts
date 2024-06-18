ad702a020f4228a5a70e89617731da4978f960c0
Armi64bit <80203683+Armi64bit@users.noreply.github.com>
2024-06-18 11:59:01 +0100

    "Added animation to icons in workshop component and implemented reveal animation on component load"

--- src/app/pages/lean/andon/workshop/workshop.component.ts
+++ src/app/pages/lean/andon/workshop/workshop.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'ngx-workshop',
   templateUrl: './workshop.component.html',
-  styleUrls: ['./workshop.component.scss']
+  styleUrls: ['./workshop.component.scss'],
+  animations: [
+    trigger('revealAnimation', [
+      state('hidden', style({
+        opacity: 0,
+        transform: 'translateY(100px)'
+      })),
+      state('visible', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+      transition('hidden => visible', [
+        animate('0.5s ease-out')
+      ])
+    ])
+  ]
 })
-export class WorkshopComponent {
+export class WorkshopComponent implements AfterViewInit {
+  isVisible = 'hidden';
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.isVisible = 'visible';
+    }, 100);
+  }
 }
